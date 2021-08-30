84e00c637812b368b1728ce0f242d6981888872a
Hasan Ablak <0hasanablak@gmail.com>
2021-08-30 12:33:14 +0300

    removeBasket

--- src/components/product.js
+++ src/components/product.js
@@ -2,6 +2,8 @@ import { useState } from "react"
 
 function Product({ product, basket, setBasket }) {
 	
+	const basketItem = basket.find(item => item.id === product.id);
+
 	const addBasket = (e) => {
 		const checkBasket = basket.find(s => s.id == product.id)
 		if (checkBasket) {
@@ -16,14 +18,31 @@ function Product({ product, basket, setBasket }) {
 		}
 	}
 
+	const removeBasket = (e) => {
+		// Item'in durumu 1 ise, itemi komple silelim,
+		// Item'in durumu 1 değil ise -1 yapalım
+		
+		const currentBasket = basket.find(s => s.id == product.id);
+		const basketWithoutCurrent = [...basket.filter(item => item.id !== product.id)];
+
+		currentBasket.amount--;
+		
+		if (currentBasket.amount == 0) {
+			// Komple silelim
+			setBasket([...basketWithoutCurrent]);
+		} else {
+			// 1 azaltalım
+			setBasket([...basketWithoutCurrent, currentBasket]);
+		}
+	}
 
 	return (
 		<div className="product">
 			<h6>{product.title}</h6>
 			<div className="price">${product.price}</div>
 			<div className="actions">
-				<button>Sat</button>
-				<span className="amount">0</span>
+				<button disabled={!basketItem} onClick={removeBasket}>Sat</button>
+				<span className="amount">{basketItem && basketItem.amount || 0}</span>
 				<button onClick={addBasket}>Satın Al</button>
 			</div>
 			<style jsx>{`
